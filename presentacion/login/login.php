2e7a10333d31c0af942bb6b916d07569d9ce0a2b
QVal123 <1646281@senati.pe>
2025-11-19 20:25:41 -0500

    se completo

---
+++ presentacion/login/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- La siguiente linea es para utilizar la hoja de estilos -->
+    <link rel="stylesheet" href="estilo.css">
+    <title>login</title>
+</head>
+<body>
+    <h1>Iniciar Sesión</h1>
+    <form action="logincontroller.php" method="post">
+        <input name="nombre_usuario" type="text" placeholder="Escribe tu nombre de usuario">
+        <br><br>
+        <input name="contraseña" type="password" placeholder="Contraseña">
+        <br><br>
+        <input type="submit" value="Iniciar sesión">
+    </form>
+</body>
+</html>
