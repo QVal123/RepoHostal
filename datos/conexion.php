3c1c943cf673ec9e1ffd21ec145a1e8d9234ea0d
unknown <janethsacae@gmail.com>
2025-11-22 23:10:25 -0500

    Resolviendo conflicto en conexion.php

--- datos/conexion.php
+++ datos/conexion.php
@@ -1,11 +1,16 @@
 <?php
     class DB{
         public function conectar(){
-            $url='mysql:host=localhost; dbname=new_chema';
-            $user='root';
-            $password='12345';
-            $cn=new PDO($url, $user, $password);
-            return $cn;
+            $url = 'mysql:host=localhost;dbname=new_chema';
+            $user = 'root';
+            $password = '12345';
+
+            try {
+                $cn = new PDO($url, $user, $password);
+                return $cn;
+            } catch (PDOException $e) {
+                die("Error de conexión: " . $e->getMessage());
+            }
         }
     }
 ?>
